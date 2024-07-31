03ada5dd44181485867987c9c74466f3b9b4eeb6
Michael Hixon <michael.hixon@hotmail.com>
2024-07-31 15:53:40 -0600

    bump version back to correct number

--- src/snow/meta.js
+++ src/snow/meta.js
@@ -4,7 +4,7 @@
 // @description  Various automations on SmartIT
 // @match       ebayinc.service-now.com/*
 // @grant       GM_addStyle
-// @version     0.2.2
+// @version     0.1.1
 // @author      process.env.AUTHOR
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/ui@0.7
