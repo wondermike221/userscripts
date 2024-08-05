4964c0b963e2bf6623d514c09ba35405390b88c8
Michael Hixon <michael.hixon@hotmail.com>
2024-08-05 16:11:22 -0600

    rebuild

--- dist/snow.user.js
+++ dist/snow.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://hixon.dev
 // @description Various automations on SmartIT
 // @match       ebayinc.service-now.com/*
-// @version     0.1.1
+// @version     0.1.2
 // @author      Michael Hixon
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/ui@0.7
